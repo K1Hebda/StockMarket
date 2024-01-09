c4f28bd7a2798d310ff03be6cbb58c9b731eb243
Kamil Hebda <kamilhebda28@gmail.com>
2024-01-09 01:25:55 +0100

    Removal of the switchToMain method.

--- src/application/SceneController.java
+++ src/application/SceneController.java
@@ -1,5 +1,8 @@
 package application;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -9,29 +12,23 @@ import javafx.stage.Stage;
 
 public class SceneController {
 
+	private static Logger logger = LogManager.getLogger(Main.class.getName());
+	
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+ // Method handling the transition to the WIG20 scene
     public void switchToWIG20(ActionEvent event) {
         try {
+        	logger.info("Switching to WIG20 scene");
             root = FXMLLoader.load(getClass().getResource("WIG20.fxml"));
             stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public void switchToMain(ActionEvent event) {
-        try {
-            root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
+        	logger.error("Switching to WIG20 scene error ", e);
             e.printStackTrace();
         }
     }
