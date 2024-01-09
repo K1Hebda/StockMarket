56ef25b6fb15c0079e4d911504769dcdabfe6c56
Kamil Hebda <kamilhebda28@gmail.com>
2024-01-09 01:31:50 +0100

    Make modifications and update styles

--- src/application/BankScraper.java
+++ src/application/BankScraper.java
@@ -13,35 +13,36 @@ import org.apache.logging.log4j.Logger;
 public class BankScraper {
 
 	private static Logger logger = LogManager.getLogger(BankScraper.class.getName());
+	public static ArrayList<ArrayList<String>> ListOfCompanies = new ArrayList<ArrayList<String>>();
 
-	public static void main(String[] args) {
+	public static void startScraper() {
 		logger.info("Using BankScraper");
-		ArrayList<ArrayList<String>> ListOfCompanies = new ArrayList<ArrayList<String>>();
 
 		try {
-			// Adres URL do strony z notowaniami WIG20
+			// URL address for the WIG20 stock quotes page
 			String url = "https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=WIG20";
+			logger.debug("URL: {}", url);
 
-			// Pobierz stronę HTML
+			// Retrieve HTML page
 			logger.info("Connecting to the URL...");
 			Document document = Jsoup.connect(url).get();
 			logger.info("Connection successful!");
 
-			// Pobierz tabelę z notowaniami
+			// Retrieve the table with stock quotes
 			Elements tableRows = document.select("table.sortTableMixedData tbody tr");
 
 			int i = 0;
-			// Iteruj przez wiersze tabeli
+			// Iterate through the table rows
 			logger.warn("Iterating through table rows");
 			for (Element row : tableRows) {
 
-				// Pobierz komórki w wierszu
+				// Retrieve cells in the row
 				Elements cells = row.select("td");
 
-				// Sprawdź, czy wiersz ma wystarczającą ilość komórek
+				// Check if the row has an adequate number of cells
 				if (cells.size() >= 9) {
 					ListOfCompanies.add(new ArrayList<String>());
-					// Pobierz dane z komórek
+					// Retrieve data from cells
 					ListOfCompanies.get(i).add(cells.get(0).select("a").text());
 
 					for (int j = 1; j < 9; j++) {
@@ -51,21 +52,9 @@ public class BankScraper {
 				}
 			}
 			logger.info("End of iterating through table rows.");
-			// Wyświetl dane
-			for (int l = 0; l < i; l++) {
-				System.out.println("Spółka: " + ListOfCompanies.get(l).get(0));
-				System.out.println("Symbol: " + ListOfCompanies.get(l).get(1));
-				System.out.println("Ostatnia cena: " + ListOfCompanies.get(l).get(2));
-				System.out.println("Zmiana: " + ListOfCompanies.get(l).get(3));
-				System.out.println("Zmiana procentowa: " + ListOfCompanies.get(l).get(4));
-				System.out.println("Wpływ na indeks: " + ListOfCompanies.get(l).get(5));
-				System.out.println("Udział w obrocie: " + ListOfCompanies.get(l).get(6));
-				System.out.println("Pakiet: " + ListOfCompanies.get(l).get(7));
-				System.out.println("Udział w portfelu: " + ListOfCompanies.get(l).get(8));
-				System.out.println("---------------------");
-			}
+
 		} catch (IOException e) {
-			logger.error("Error during connecting with URL or saving data" + e);
+			logger.error("Error during connecting with URL or saving data", e);
 			e.printStackTrace();
 		}
 		logger.info("End of Using BankScraper");
--- src/application/Main.java
+++ src/application/Main.java
@@ -5,18 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
@@ -30,18 +23,6 @@ public class Main extends Application {
 			Scene scene = new Scene(root, 600, 400, Color.AZURE);
 			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			Image icon = new Image("logo.jpg");
-
-			
-			/*
-			 * Text text = new Text(); text.setText("Pablo Escobar"); text.setX(400);
-			 * text.setY(50); text.setFont(Font.font("Verdana", 30));
-			 * text.setFill(Color.GREY);
-			 * 
-			 * Image image = new Image("pablo.jpg"); ImageView imageView = new
-			 * ImageView(image); imageView.setX(100); imageView.setY(100);
-			 * 
-			 * root.getChildren().add(text); root.getChildren().add(imageView);
-			 */
 
 			stage.getIcons().add(icon);
 			stage.setTitle("Aplikacja giełdowa");
