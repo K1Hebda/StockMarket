a4d4893712ed6a306fff710256f08ba2109454af
Kamil Hebda <kamilhebda28@gmail.com>
2024-01-14 17:43:06 +0100

    Adding buttons that change data on the chart and in the text field

--- src/application/Wig20Controller.java
+++ src/application/Wig20Controller.java
@@ -1,6 +1,5 @@
 package application;
 
-import java.awt.Button;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,6 +20,8 @@ import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
@@ -35,8 +36,68 @@ public class Wig20Controller {
 	private Parent root;
 
 	@FXML
+	private Button alior;
+
+	@FXML
+	private Button allegro;
+
+	@FXML
+	private Button asseco;
+
+	@FXML
+	private Button cdProjekt;
+
+	@FXML
+	private Button dino;
+
+	@FXML
+	private Button gk;
+
+	@FXML
+	private Button jswaSA;
+
+	@FXML
+	private Button kghm;
+
+	@FXML
+	private Button kruk;
+
+	@FXML
+	private Button mbank;
+
+	@FXML
+	private Button orange;
+
+	@FXML
+	private Button orlen;
+
+	@FXML
+	private Button pekao;
+
+	@FXML
+	private Button pepco;
+
+	@FXML
+	private Button pge;
+
+	@FXML
+	private Button pko;
+
+	@FXML
+	private Button polsat;
+
+	@FXML
+	private Button pzu;
+
+	@FXML
+	private Button lpp;
+
+	@FXML
+	private Button santander;
+
+	@FXML
 	private TextArea outputTextArea1;
-	
+
 	@FXML
 	private TextArea outputTextArea2;
 
@@ -78,27 +139,39 @@ public class Wig20Controller {
 
 	@FXML
 	private NumberAxis yAxis;
+	
+	@FXML
+    private Label companyNameLabel;
 
 	public void initialize() {
-	    ExecutorService executorService = Executors.newFixedThreadPool(3);
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+		companyNameLabel.setText("WIG20");
 
-	    try {
-	        // Running each method in a separate CompletableFuture, executing in parallel
-	        CompletableFuture<Void> chartFuture = CompletableFuture.runAsync(this::fetchDataForChart, executorService);
-	        CompletableFuture<Void> tableFuture = CompletableFuture.runAsync(this::fetchDataForTableView, executorService);
-	        CompletableFuture<Void> textAreaFuture = CompletableFuture.runAsync(this::fetchDataForTextArea, executorService);
-	        chartFuture.get();
-	        tableFuture.get();
-	        textAreaFuture.get();
-	    } catch (Exception e) {
-	        logger.error("Error with threads", e);
-	    } finally {
-	        executorService.shutdown();
-	    }
+		try {
+			// Running each method in a separate CompletableFuture, executing in parallel
+			// Asynchronously execute the fetchDataToChart method with the argument "WIG20"
+			// using CompletableFuture
+			CompletableFuture<Void> chartFuture = CompletableFuture.runAsync(() -> fetchDataToChart("WIG20"),
+					executorService);
+			CompletableFuture<Void> tableFuture = CompletableFuture.runAsync(this::fetchDataForTableView,
+					executorService);
+			CompletableFuture<Void> textAreaFuture = CompletableFuture.runAsync(()->fetchDataForTextArea("WIG20"),
+					executorService);
+			chartFuture.get();
+			tableFuture.get();
+			textAreaFuture.get();
+		} catch (Exception e) {
+			logger.error("Error with threads", e);
+		} finally {
+			executorService.shutdown();
+		}
 	}
 
-	private void fetchDataForTextArea() {
-		BankScraper.startWIGInformationScraper();
+	private void fetchDataForTextArea(String company) {
+		logger.info("Fetching data for TextArea for company: {}", company);
+		outputTextArea1.clear();
+		outputTextArea2.clear();
+		BankScraper.startWIGInformationScraper(company);
 		logger.warn("Fetching data for the table");
 
 		StringBuilder output1 = new StringBuilder();
@@ -113,9 +186,10 @@ public class Wig20Controller {
 			if (outputTextArea1 != null && outputTextArea2 != null) {
 				outputTextArea1.setText(output1.toString());
 				outputTextArea2.setText(output2.toString());
-				
+				logger.info("Text set in TextArea successfully.");
+
 			} else {
-				System.err.println("TextArea is null. Make sure it's properly initialized.");
+				logger.error("TextArea is null. Make sure it's properly initialized.");
 			}
 		} catch (Exception e) {
 			logger.error("Error setting text in TextArea", e);
@@ -157,8 +231,9 @@ public class Wig20Controller {
 		}
 	}
 
-	private void fetchDataForChart() {
-		BankScraper.startInformationToChartScraper();
+	private void fetchDataToChart(String company) {
+		logger.info("Fetching data for chart for company: {}", company);
+		BankScraper.startInformationToChartScraper(company);
 		XYChart.Series<String, Number> series = new XYChart.Series<>();
 		yAxis.setAutoRanging(false);
 
@@ -172,42 +247,147 @@ public class Wig20Controller {
 			}
 		}
 
+		// setting the y-axis range
 		yAxis.setLowerBound(Collections.min(BankScraper.chartYValue) - 5);
 		yAxis.setUpperBound(Collections.max(BankScraper.chartYValue) + 5);
 		wig20Chart.getData().add(series);
+		logger.info("Chart data added successfully for company: {}", company);
+	}
+
+	
+	@FXML
+	private void handleButtonAction(ActionEvent event) {
+		BankScraper.chartXValue.clear();
+		BankScraper.chartYValue.clear();
+		wig20Chart.getData().clear();
+		Button clickedButton = (Button) event.getSource();
+		
+		 logger.info("Button clicked: {}", clickedButton.getId());
+
+		switch (clickedButton.getId()) {
+		case "alior":
+	        companyNameLabel.setText("ALIOR BANK");
+	        fetchDataToChart("ALIOR-BANK");
+	        fetchDataForTextArea("ALIOR");
+	        break;
+	    case "allegro":
+	        companyNameLabel.setText("ALLEGRO");
+	        fetchDataToChart("ALE");
+	        fetchDataForTextArea("ALLEGRO");
+	        break;
+	    case "asseco":
+	        companyNameLabel.setText("ASSECO POLAND");
+	        fetchDataToChart("ASSECO-POLAND");
+	        fetchDataForTextArea("ASSECOPOL");
+	        break;
+	    case "cdProjekt":
+	        companyNameLabel.setText("CD PROJEKT");
+	        fetchDataToChart("CD-PROJEKT");
+	        fetchDataForTextArea("CDPROJEKT");
+	        break;
+	    case "dino":
+	        companyNameLabel.setText("DINO");
+	        fetchDataToChart("DNP");
+	        fetchDataForTextArea("DINOPL");
+	        break;
+	    case "gk":
+	        companyNameLabel.setText("GRUPA KĘTY");
+	        fetchDataToChart("KETY");
+	        fetchDataForTextArea("KETY");
+	        break;
+	    case "jswaSA":
+	        companyNameLabel.setText("JASTRZĘBSKA SPÓŁKA WĘGLOWA");
+	        fetchDataToChart("JSW-JASTRZEBSKA-SPOLKA-WEGLOWA");
+	        fetchDataForTextArea("JSW");
+	        break;
+	    case "kghm":
+	        companyNameLabel.setText("KGHM POLSKA MIEDZ");
+	        fetchDataToChart("KGHM");
+	        fetchDataForTextArea("KGHM");
+	        break;
+	    case "kruk":
+	        companyNameLabel.setText("KRUK");
+	        fetchDataToChart("KRUK");
+	        fetchDataForTextArea("KRUK");
+	        break;
+	    case "mbank":
+	        companyNameLabel.setText("MBANK");
+	        fetchDataToChart("MBANK");
+	        fetchDataForTextArea("MBANK");
+	        break;
+	    case "orange":
+	        companyNameLabel.setText("ORANGE POLSKA");
+	        fetchDataToChart("ORANGE");
+	        fetchDataForTextArea("ORANGEPL");
+	        break;
+	    case "orlen":
+	        companyNameLabel.setText("PKN ORLEN");
+	        fetchDataToChart("ORLEN");
+	        fetchDataForTextArea("PKNORLEN");
+	        break;
+	    case "pekao":
+	        companyNameLabel.setText("PEKAO, BANK POLSKA KASA OPIEKI");
+	        fetchDataToChart("PEKAO");
+	        fetchDataForTextArea("PEKAO");
+	        break;
+	    case "pepco":
+	        companyNameLabel.setText("PEPCO GROUP");
+	        fetchDataToChart("PCO");
+	        fetchDataForTextArea("PEPCO");
+	        break;
+	    case "pge":
+	        companyNameLabel.setText("PGE, POLSKA GRUPA ENERGETYCZNA");
+	        fetchDataToChart("PGE");
+	        fetchDataForTextArea("PGE");
+	        break;
+	    case "pko":
+	        companyNameLabel.setText("PKO, POWSZECHA KASA OSZCZĘDNOŚCI BANK POLSKI");
+	        fetchDataToChart("PKO");
+	        fetchDataForTextArea("PKOBP");
+	        break;
+	    case "polsat":
+	        companyNameLabel.setText("CYFROWY POLSAT");
+	        fetchDataToChart("CYFROWY-POLSAT");
+	        fetchDataForTextArea("CYFRPLSAT");
+	        break;
+	    case "pzu":
+	        companyNameLabel.setText("PZU, POWSZECHNY ZAKŁAD UBEZPIECZEŃ");
+	        fetchDataToChart("PZU");
+	        fetchDataForTextArea("PZU");
+	        break;
+	    case "santander":
+	        companyNameLabel.setText("SANTANDER BANK POLSKA");
+	        fetchDataToChart("SPL");
+	        fetchDataForTextArea("SANPL");
+	        break;
+	    case "lpp":
+	        companyNameLabel.setText("LPP");
+	        fetchDataToChart("LPP");
+	        fetchDataForTextArea("LPP");
+	        break;
+	    default:
+	        companyNameLabel.setText("Unrecognized Company");
+	        logger.warn("Unrecognized button: {}", clickedButton.getId());
+	        break;
+
+		}
 	}
 
 	public void switchToMain(ActionEvent event) {
-	    logger.info("Switching to the Main scene");
-	    try {
-	        BankScraper.ListOfCompanies.clear();
-	        logger.debug("Clearing ListOfCompanies");
+		logger.info("Switching to the Main scene");
+		try {
+			BankScraper.ListOfCompanies.clear();
+			logger.debug("Clearing ListOfCompanies");
 
-	        // Loading the Main.fxml file and switch to the Main scene
-	        root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-	        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	        scene = new Scene(root);
-	        stage.setScene(scene);
-	        stage.show();
-	    } catch (IOException e) {
-	        logger.error("Error while switching to the Main scene", e);
-	    }
+			// Loading the Main.fxml file and switch to the Main scene
+			root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			logger.error("Error while switching to the Main scene", e);
+		}
 	}
 
-	/*
-	 * @FXML private void switchToCompanyChart(ActionEvent event) { try { Button
-	 * clickedButton = (Button) event.getSource(); String companyName = (String)
-	 * clickedButton.getUserData();
-	 * 
-	 * // Wywołaj metodę, która ustawia wykres dla danej spółki
-	 * setCompanyChart(companyName); } catch (Exception e) {
-	 * logger.error("Error switching to company chart", e); } }
-	 */
-	/*
-	 * private void handleOpenSecondWindow() { try { FXMLLoader loader = new
-	 * FXMLLoader(getClass().getResource("ChartCompany.fxml")); Parent root =
-	 * loader.load(); Stage secondStage = new Stage(); secondStage.setScene(new
-	 * Scene(root)); secondStage.show(); } catch (Exception e) {
-	 * logger.error("Error opening the second window", e); e.printStackTrace(); } }
-	 */
 }
